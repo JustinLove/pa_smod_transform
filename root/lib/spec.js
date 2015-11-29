f6a70a363fc983473c870b19788467623c16f941
Justin Love <git@JustinLove.name>
2015-11-29 10:39:17 -0600

    _ex1 adjustments and more pa path locations

--- root/lib/spec.js
+++ root/lib/spec.js
@@ -2,22 +2,22 @@ module.exports.copyPairs = function(grunt, files, media) {
   media = media || ''
   var allSpecs = grunt.file.expand({cwd: media}, files)
   return allSpecs.map(function(relpath) {
-    return {relpath: relpath, abspath: media + relpath}
+    return {relpath: relpath.replace('_ex1', ''), abspath: media + relpath}
   })
 }
 
 module.exports.specFiles = function(grunt, filename_regexp, media) {
   media = media || ''
   var allSpecs = grunt.file.expand({cwd: media}, [
-    'pa/ammo/**/*.json',
-    'pa/tools/**/*.json',
-    'pa/units/**/*.json'
+    'pa*/ammo/**/*.json',
+    'pa*/tools/**/*.json',
+    'pa*/units/**/*.json'
   ])
   var filter = new RegExp(filename_regexp, '')
   var specs = []
   allSpecs.forEach(function(relpath) {
     if (filter.test(relpath)) {
-      specs.push({relpath: relpath, abspath: media + relpath})
+      specs.push({relpath: relpath.replace('_ex1', ''), abspath: media + relpath})
     }
   })
   return specs
